6bd59399f6923e39e5cf57fd18b439aba87f5558
mason00 <masondev@gmail.com>
2016-08-07 20:05:10 +1000

    edit

---
+++ iasset/Scripts/app/flightController.js
@@ -0,0 +1,19 @@
+﻿angular.module("iassetApp", ['ngRoute'])
+    .controller("flightController", function ($scope, $http, $routeParams) {
+        $scope.Title = "Edit flight";
+        $scope.FlightNo = $routeParams.flightNo;
+
+        init = function () {
+            angular.forEach($scope.currentGate.Flights, function (val, key) {
+                if (val.FlightNo == $scope.FlightNo) {
+                    $scope.currentFlight = val;
+                }
+            });
+        }
+
+        init();
+
+        $scope.save = function (flight) {
+            console.log(flight);
+        };
+    });
---
+++ iasset/Scripts/app/gateController.js
@@ -0,0 +1,37 @@
+﻿angular.module("iassetApp", ['ngRoute'])
+.controller('gateController', function ($scope, $http, $routeParams) {
+    $scope.Title = "Gate management app";
+
+        
+    $scope.unAssignedFlights = [];
+
+    console.log($routeParams.flightNo);
+
+    $scope.cancel = function (FlightNo) {
+        $scope.currentGate.Flights = $scope.currentGate.Flights.filter(function (flight) {
+            var result = flight.FlightNo != FlightNo;
+            if (!result) {
+                $scope.unAssignedFlights.push(flight);
+            }
+            return result;
+        });
+    };
+
+    $scope.edit = function () {
+        console.log($routeParams.flightNo)
+    }
+
+    $scope.select = function (gate) {
+        $scope.currentGate = gate;
+    };
+
+    $scope.AddToCurrentGate = function (FlightNo) {
+        var no = FlightNo;
+        $scope.unAssignedFlights = $scope.unAssignedFlights.filter(function (flight) {
+            if (flight.FlightNo == no) {
+                $scope.currentGate.Flights.push(flight);
+            }
+            return flight.FlightNo != no;
+        });
+    }
+});
